4d4d0b4cef75af319197e13b4280b49cc374ae81
AgustinQuiroz98 <agu.quiroz@gmail.com>
2022-02-04 15:02:42 -0300

    navBar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./styles.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navBar">
+      <Link className="links" to={"/"}>
+        <h1>MERNstaurant</h1>
+      </Link>
+      <Link id="navLinks" className="links" to={"/"}>
+        Promotions
+      </Link>
+      <Link className="links" to={"/"}>
+        Menu
+      </Link>
+      <Link className="links" to={"/"}>
+        Reserve
+      </Link>
+      <Link className="links" to={"/"}>
+        Gallery
+      </Link>
+      <Link className="links" to={"/"}>
+        Drinks
+      </Link>
+      <Link className="links" to={"/"}>
+        Happy Hours
+      </Link>
+      <Link className="links" to={"/"}>
+        Party
+      </Link>
+      <Link className="links" to={"/"}>
+        About Us
+      </Link>
+    </div>
+  );
 };
 
 export default Navbar;
