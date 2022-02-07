c51205e1458b31590dd5ca507ae8f00c9af329eb
Erwin Meier <emeier.duguet@gmail.com>
2022-02-07 19:56:59 -0300

    componente reserva

---
+++ client/src/components/ReservaForm.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import validation from './actions/validation'
+
+const ReservaForm = ({ personas, turnoId }) => {
+    const [nameValidation, setNameValidation] = useState({ valid: false, text: null });
+    const [lastNameValidation, setLastNameValidation] = useState({ valid: false, text: null });
+    const [emailValidation, setEmailValidation] = useState({ valid: false, text: null });
+    
+    const validate = (e) => {
+        switch (e.target.name) {
+            case "name":
+                setNameValidation(validation(e.target.name, e.target.value));
+                break;
+            case "lastName":
+                setLastNameValidation(validation(e.target.name, e.target.value));
+                break;
+            case "email":
+                setEmailValidation(validation(e.target.name, e.target.value));
+                break;
+            default:
+                return null;
+        }
+
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        const formEl = e.target.closest('form');
+            const formData = new FormData(formEl);
+            const entries = {};
+            for (let [key, value] of formData.entries()) entries[key] = value.trim();
+            entries.personas = personas;
+            entries.turnoId = turnoId;
+            console.log(entries)
+        console.log(nameValidation.valid)
+        if (nameValidation.valid && lastNameValidation.valid && emailValidation.valid) {
+            try {
+                const res = await axios.post('/api/reserva/create', entries);
+                console.log(res);
+                alert("Reservado!")
+                e.target.reset();
+            } catch (err) {
+                console.error(err);
+                alert("Error, try again")
+            }
+        } else {
+            alert("Form doesn't match all requirements")
+        }
+    }
+
+    return (
+        <form className='register' onSubmit={onSubmit}>
+            <div><label htmlFor='name'>Name</label> {nameValidation.text}</div>
+            <input type="text" id="name" name="name" onChange={validate} />
+            <div><label htmlFor='lastName'>Lastname</label> {lastNameValidation.text}</div>
+            <input type="text" id="lastName" name="lastName" onChange={validate} />
+            <div><label htmlFor='email'>Email</label> {emailValidation.text}</div>
+            <input type="email" id="email" name="email" onChange={validate} />
+            <button>Reservar</button>
+        </form>
+    );
+}
+
+export default ReservaForm;
--- client/src/components/Reserve.js
+++ client/src/components/Reserve.js
@@ -1,7 +1,91 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import DatePicker, { registerLocale } from "react-datepicker";
+import es from "date-fns/locale/es";
+import "react-datepicker/dist/react-datepicker.css";
+import setHours from "date-fns/setHours";
+import setMinutes from "date-fns/setMinutes";
+import axios from "axios";
+import ReservaForm from './ReservaForm';
+registerLocale("es", es);
 
 const Reserve = () => {
-  return <div>Reserve</div>;
-};
+    const [startDate, setStartDate] = useState(setHours(setMinutes(new Date(), 0), 14));
+    const [disponible, setDisponible] = useState(false);
+    const [personas, setPersonas] = useState(2);
+    const [turnoId, setTurnoId] = useState("");
+    const [times, setTimes] = useState([]);
+
+    useEffect(() => {
+        const hora = new Date().getHours();
+        const includedTimes = [];
+        for (let i = 14; i <= 22; i += 2) {
+            includedTimes.push(setHours(setMinutes(new Date(), 0), i))
+        }
+        setTimes(includedTimes);
+
+        const uuid = startDate.getFullYear().toString() + ("0" + (startDate.getMonth() + 1)).slice(-2).toString() +
+            ("0" + startDate.getDate()).slice(-2).toString() + startDate.getHours().toString() + "00";
+        const check = async () => {
+            try {
+                const turno = await axios.post('/api/reserva/check', { turnoId: uuid, personas: personas });
+                console.log(turno.data.available)
+
+                setDisponible(turno.data.available);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        check();
+    }, []);
+
+    const onDatePick = async (date) => {
+        setStartDate(date);
+        const uuid = date.getFullYear().toString() + ("0" + (date.getMonth() + 1)).slice(-2).toString() +
+            ("0" + date.getDate()).slice(-2).toString() + date.getHours().toString() + "00";
+        console.log(uuid);
+        setTurnoId(uuid);
+
+        try {
+            const turno = await axios.post('/api/reserva/check', { turnoId: uuid, personas: personas });
+            console.log(turno.data.available)
+
+            setDisponible(turno.data.available);
+        } catch (err) {
+            console.log(err);
+            setDisponible(false);
+        }
+    }
+
+    const personasChange = async (e) => {
+        e.preventDefault();
+        const p = e.target.value;
+        setPersonas(p);
+        try {
+            const turno = await axios.post('/api/reserva/check', { turnoId: turnoId, personas: p });
+            console.log(turno.data.available)
+
+            setDisponible(turno.data.available);
+        } catch (err) {
+            console.log(err);
+            setDisponible(false);
+        }
+    }
+    return (
+        <>
+            <input type={"number"} defaultValue={personas} min={"2"} max={"8"} onChange={e => personasChange(e)} />
+            <DatePicker
+                selected={startDate}
+                onChange={date => onDatePick(date)}
+                minDate={new Date()}
+                locale="es"
+                withPortal
+                showTimeSelect
+                includeTimes={times}
+                dateFormat="MMMM d, yyyy h:mm aa" />
+            {disponible ? <ReservaForm personas={personas} turnoId={turnoId} />
+                : null}
+        </>
+    );
+}
 
 export default Reserve;
