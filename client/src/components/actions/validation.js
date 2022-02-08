bb7b9f2fa986ffad0878c088f62a3c4465719a37
Erwin Meier <emeier.duguet@gmail.com>
2022-02-07 23:30:29 -0300

    validation a español

--- client/src/components/actions/validation.js
+++ client/src/components/actions/validation.js
@@ -2,19 +2,19 @@ const validation = (obj, val) => {
     switch (obj) {
         case 'name':
             if (val.length < 2) {
-                return { valid: false, text: <p>Name should have at least 2 characters</p> }
+                return { valid: false, text: <p>nombre debe tener al menos 2 letras</p> }
             } else {
                 return { valid: true, text: null };
             }
         case "lastName":
             if (val.length < 2) {
-                return { valid: false, text: <p>Lastname should have at least 2 characters</p> }
+                return { valid: false, text: <p>apellido debe tener al menos 2 letras</p> }
             } else {
                 return { valid: true, text: null };
             }
         case "email":
             if (!/^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(val)) {
-                return { valid: false, text: <p>Invalid email</p> }
+                return { valid: false, text: <p>correo inválido</p> }
             } else {
                 return { valid: true, text: null };
             }
