6baca83ad563d9e2ec1ddf04f05cf3a90ce52eae
edureyesm <edu.reyesmartinez@gmail.com>
2022-02-09 17:16:15 -0300

    Mejor estilo

--- client/src/views/NotFound.js
+++ client/src/views/NotFound.js
@@ -4,17 +4,16 @@ import { Button, Container } from "react-bootstrap";
 
 const Notfound = () => {
   return (
-    <Container
-      fluid
-      className="d-flex text-center text-secondary cover-container"
-    >
-      <main className="px-3 py-5">
-        <h1>¿Te perdiste?</h1>
-        <p class="lead">No te preocupes, te ayudamos a volver.</p>
-        <Button as={Link} to={"/"} className="custom-primary">
-          Volver al Home
-        </Button>
-      </main>
+    <Container fluid className="d-flex text-center text-secondary cover-container">
+      <Container className="d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <main className="px-3 lost">
+          <h1>¿Te perdiste?</h1>
+          <p class="lead">No te preocupes, te ayudamos a volver.</p>
+          <Button as={Link} to={"/"} className="custom-primary">
+            Volver al Home
+          </Button>
+        </main>
+      </Container>
     </Container>
   );
 };
