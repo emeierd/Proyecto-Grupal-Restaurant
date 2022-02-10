1973c4cbf25f83842856d2d21062418a62a3ca20
edureyesm <edu.reyesmartinez@gmail.com>
2022-02-10 16:06:44 -0300

    Home Terminado

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -1,28 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FaFacebookF, FaInstagram, FaWhatsapp } from "react-icons/fa";
+import { Container, Col, Row } from "react-bootstrap";
 
 const AboutUs = () => {
   return (
-    <div className="text-primary text-center bg-dark">
-      <h1>¿Quienes Somos?</h1>
-      <p className="text-secondary">
-        Somos una Pizzeria inspirada en la cocina de MERN Ramsay, contamos
-        actualmente con un local con aforo para 80 personas ubicado en{" "}
-        <div className="text-light">Av. Suecia #1234, Providencia.</div>Y si
-        deseas probar nuestras Pizzas desde la comodidad de tu casa, tambien
-        contamos con un servicio{" "}
-        <Link className="text-light" to="/order">
-          delivery.
-        </Link>
-        <br />
-        Ante cualquier pregunta no dudes en contactarnos via redes sociales
-      </p>
-      <div className="text-light">
-        <FaWhatsapp fontSize={27} className="mx-3" />
-        <FaFacebookF fontSize={27} className="mx-3" />
-        <FaInstagram fontSize={27} className="mx-3" />
-      </div>
+    <div className="text-primary text-center aboutus">
+      <Row>
+        <Col md={6} className="aboutus-cover"></Col>
+        <Col md={6}>
+        <Container fluid className="py-5 px-5 aboutus-text">
+          <h1>¿Quienes Somos?</h1>
+          <p className="text-light">
+            Somos una Pizzeria inspirada en la cocina de MERN Ramsay, contamos
+            actualmente con un local con aforo para 80 personas ubicado en{" "}
+            <span className="text-info">Av. Suecia #1234, Providencia.</span>Y si
+            deseas probar nuestras Pizzas desde la comodidad de tu casa, tambien
+            contamos con un servicio{" "}
+            <Link className="text-info" to="/order">
+              delivery.
+            </Link>
+            <br />
+            Ante cualquier pregunta no dudes en contactarnos via redes sociales
+          </p>
+          </Container>
+        </Col>
+      </Row>
     </div>
   );
 };
